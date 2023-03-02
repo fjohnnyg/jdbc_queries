4e485539e4c398b0758536db6bb7bc2b62155c97
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-03-02 15:26:24 +0000

    table creation

--- src/CustomerRepository.java
+++ src/CustomerRepository.java
@@ -1,26 +1,26 @@
 import java.util.List;
 public class CustomerRepository {
-
-    void createCustomer(Customer customer) {
-        // INSERT INTO Customers (name, phone_number, email) VALUES (?,?,?)
-    }
-
-    Customer  getCustomer(int id) {
-        // SELECT * FROM Customers WHERE id = ?
-        return null;
-    }
-
-    Customer  updateCustomer(Customer customer) {
-        // UPDATE Customers SET name = ?, phone_number = ?, email = ? WHERE id = ?
-        return null;
-    }
-
-    void   deleteCustomer(int id) {
-        // DELETE FROM Customers WHERE id = ?
-    }
-
-    List<Customer> getCustomers() {
-        // SELECT * FROM Customers
-        return null;
-    }
+//
+//    void createCustomer(Customer customer) {
+//        // INSERT INTO Customers (name, phone_number, email) VALUES (?,?,?)
+//    }
+//
+//    Customer  getCustomer(int id) {
+//        // SELECT * FROM Customers WHERE id = ?
+//        return null;
+//    }
+//
+//    Customer  updateCustomer(Customer customer) {
+//        // UPDATE Customers SET name = ?, phone_number = ?, email = ? WHERE id = ?
+//        return null;
+//    }
+//
+//    void   deleteCustomer(int id) {
+//        // DELETE FROM Customers WHERE id = ?
+//    }
+//
+//    List<Customer> getCustomers() {
+//        // SELECT * FROM Customers
+//        return null;
+//    }
 }
---
+++ src/MainTest.java
@@ -0,0 +1,45 @@
+import java.sql.*;
+
+public class MainTest {
+    static final String DB_URL = "jdbc:mysql://localhost/CarRentals";
+    static final String USER = "root";
+    static final String PASS = "secret";
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            // Open a connection
+            System.out.println("Connecting to database...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            // Execute a query to create a table
+            System.out.println("Creating table in given database...");
+            statement = connection.createStatement();
+            String sql = "CREATE TABLE Customers " +
+                    "(id INT NOT NULL AUTO_INCREMENT, " +
+                    "name VARCHAR(255) NOT NULL, " +
+                    "email VARCHAR(255) UNIQUE NOT NULL," +
+                    "PRIMARY KEY (id))";
+
+            statement.executeUpdate(sql);
+            System.out.println("Table created successfully...");
+        } catch (SQLException se) {
+            // Handle errors for JDBC
+            se.printStackTrace();
+        } finally {
+            // Finally block to close resources
+            try {
+                if (statement != null)
+                    connection.close();
+            } catch (SQLException se) {
+            } // do nothing
+            try {
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            } // end finally try
+        } // end try
+        System.out.println("Goodbye!");
+    } // end main
+} // end CreateTableExample
